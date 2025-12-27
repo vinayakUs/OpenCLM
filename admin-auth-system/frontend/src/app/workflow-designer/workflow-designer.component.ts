7a2970ee28e6d63fdd6f99eb8931bb8c6eef23c2
vinayakUs <vinayak200054321@gmail.com>
2025-12-27 21:27:51 +0530

    added pagination

--- admin-auth-system/frontend/src/app/workflow-designer/workflow-designer.component.ts
+++ admin-auth-system/frontend/src/app/workflow-designer/workflow-designer.component.ts
@@ -16,20 +16,38 @@ export class WorkflowDesignerComponent implements OnInit {
   private router = inject(Router);
 
   workflows = signal<Workflow[]>([]);
+  currentPage = signal(0);
+  totalPages = signal(0);
+  pageSize = signal(5); // Configurable page size
 
   ngOnInit() {
     this.loadWorkflows();
   }
 
   loadWorkflows() {
-    this.apiService.getWorkflows(0, 100).subscribe({
+    this.apiService.getWorkflows(this.currentPage(), this.pageSize()).subscribe({
       next: (response) => {
         if (response.success && response.data) {
           this.workflows.set(response.data.content);
+          this.totalPages.set(response.data.totalPages);
         }
       },
       error: (error) => console.error('Error fetching workflows:', error)
     });
+  }
+
+  nextPage() {
+    if (this.currentPage() < this.totalPages() - 1) {
+      this.currentPage.update(page => page + 1);
+      this.loadWorkflows();
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage() > 0) {
+      this.currentPage.update(page => page - 1);
+      this.loadWorkflows();
+    }
   }
 
   createNew() {
